c550602e2d0f2a1a273834595afaf6e9150eef28
fontend <yuwadee14052547@gmail.com>
2026-03-19 18:52:16 +0700

    remove env from git

--- config/pg.js
+++ config/pg.js
@@ -1,12 +1,20 @@
-import pg from "pg";
-const { Pool } = pg;
+// import pg from "pg";
+// const { Pool } = pg;
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "student",
+//   password: "Yu-14052547",
+//   port: 5432,
+// });
+
+// export default pool;
+import pkg from "pg";
+const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "student",
-  password: "Yu-14052547",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
